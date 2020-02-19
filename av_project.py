4c6a2e9406bb33fb735d3c15ebfcc264917002a2
louisccc <louis29418401@gmail.com>
2020-02-18 22:05:58 -0800

    submit skeletons

---
+++ av_project.py
@@ -0,0 +1,18 @@
+from graph_visualizer import GraphVisualizer
+# this file is skeleton file 
+
+def system_video_prepropcess(video_path):
+	# taking a directory path as input 
+	# returning a chopped video clips.
+	pass
+
+def system_graph_extraction(img_path):
+	# taking a path for an image. 
+	# should be returning graph.
+	pass
+
+def system_visualize_graph(graph, video_path):
+
+	gv = GraphVisualizer("input/lane-change/")
+    gv.show()
+
---
+++ lane_change.py
@@ -0,0 +1,7 @@
+# this script is implemented by referencing to https://github.com/carla-simulator/scenario_runner
+
+# behavior lane change. 
+
+def scenario_lane_change():
+	#TODO
+	pass

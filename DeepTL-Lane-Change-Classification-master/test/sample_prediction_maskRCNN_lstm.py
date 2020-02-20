e17d30b4f8e327fa7f504ddefba01680e00c1ee9
eslam <eslam@uci.edu>
2020-02-20 10:35:37 -0800

    nagoya dir

---
+++ DeepTL-Lane-Change-Classification-master/test/sample_prediction_maskRCNN_lstm.py
@@ -0,0 +1,21 @@
+
+from keras.models import load_model
+from lane_change_risk_detection.dataset import DataSet
+from Mask_RCNN.mask_rcnn.detect_objects import DetectObjects
+import os
+
+dir_name = os.path.dirname(__file__)
+dir_name = os.path.dirname(dir_name)
+
+image_path = os.path.join(dir_name, 'data/input/')
+masked_image_path =os.path.join(dir_name, 'data/masked_images/')
+
+masked_image_extraction = DetectObjects(image_path, masked_image_path)
+masked_image_extraction.save_masked_images()
+
+data = DataSet()
+data.read_video(masked_image_path, option='fixed frame amount', number_of_frames=50, scaling='scale', scale_x=0.1, scale_y=0.1)
+
+model = load_model('maskRCNN_CNN_lstm_GPU.h5')
+print(' safe | dangerous \n', model.predict_proba(data.video))
+

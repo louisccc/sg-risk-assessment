f6c148e037380273d8940f5cf70fc288b32049df
Arnav Malawade <malawada@uci.edu>
2020-03-04 17:40:05 -0800

    Create relation_extractor.py

---
+++ relation_extractor.py
@@ -0,0 +1,52 @@
+
+#This class extracts relations for every pair of entities in a scene
+class RelationExtractor:
+
+def __init__():
+    pass
+    
+
+#takes in two entities and extracts all relations between those two entities    
+def extract_relations(actor1, actor2):
+    pass
+    
+
+#specific relations for each pair of actors possible 
+def extract_relations_car_car(actor1, actor2):
+    pass
+def extract_relations_car_lane(actor1, actor2):
+    pass
+def extract_relations_car_light(actor1, actor2):
+    pass
+def extract_relations_car_sign(actor1, actor2):
+    pass
+def extract_relations_car_ped(actor1, actor2):
+    pass
+def extract_relations_car_bicycle(actor1, actor2):
+    pass
+def extract_relations_car_motorcycle(actor1, actor2):
+    pass
+def extract_relations_motorcycle_lane(actor1, actor2):
+    pass
+def extract_relations_motorcycle_light(actor1, actor2):
+    pass
+def extract_relations_motorcycle_sign(actor1, actor2):
+    pass
+def extract_relations_ped_lane(actor1, actor2):
+    pass
+def extract_relations_ped_light(actor1, actor2):
+    pass
+def extract_relations_ped_sign(actor1, actor2):
+    pass
+def extract_relations_bicycle_lane(actor1, actor2):
+    pass
+def extract_relations_bicycle_light(actor1, actor2):
+    pass
+def extract_relations_bicycle_sign(actor1, actor2):
+    pass
+def extract_relations_lane_lane(actor1, actor2):
+    pass
+def extract_relations_lane_sign(actor1, actor2):
+    pass
+def extract_relations_lane_lane(actor1, actor2):
+    pass

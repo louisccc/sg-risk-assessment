c8efed97ef5140caa81e20b6d78d5264445dc7d3
louisccc <louis29418401@gmail.com>
2020-03-27 21:05:09 -0700

    removing unused sensor and enable light state information extraction.

--- dataset_preparation-0.9.8/sensors.py
+++ dataset_preparation-0.9.8/sensors.py
@@ -59,114 +59,12 @@ def get_actor_attributes(actor, waypoint=None):
 def get_vehicle_attributes(vehicle, waypoint=None):
     return_dict = get_actor_attributes(vehicle, waypoint)
     
-#WILL ONLY WORK ON CARLA 0.9.8:
-    # light_state = vehicle.get_light_state()
-    # #light_state variables are booleans
-    # return_dict['left_blinker_on'] = light_state.LeftBlinker
-    # return_dict['right_blinker_on'] = light_state.RightBlinker
-    # return_dict['brake_light_on'] = light_state.Brake
+    light_state = vehicle.get_light_state()
+    #light_state variables are booleans
+    return_dict['left_blinker_on'] = light_state.LeftBlinker
+    return_dict['right_blinker_on'] = light_state.RightBlinker
+    return_dict['brake_light_on'] = light_state.Brake
     return return_dict
-
-# ==============================================================================
-# -- CollisionSensor -----------------------------------------------------------
-# ==============================================================================
-
-
-class CollisionSensor(object):
-    def __init__(self, parent_actor):
-        self.sensor = None
-        self.history = []
-        self._parent = parent_actor
-        world = self._parent.get_world()
-        bp = world.get_blueprint_library().find('sensor.other.collision')
-        self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
-        # We need to pass the lambda a weak reference to self to avoid circular
-        # reference.
-        weak_self = weakref.ref(self)
-        self.sensor.listen(lambda event: CollisionSensor._on_collision(weak_self, event))
-
-    def get_collision_history(self):
-        history = collections.defaultdict(int)
-        for frame, intensity in self.history:
-            history[frame] += intensity
-        return history
-
-    @staticmethod
-    def _on_collision(weak_self, event):
-        self = weak_self()
-        if not self:
-            return
-        actor_type = get_actor_display_name(event.other_actor)
-        impulse = event.normal_impulse
-        intensity = math.sqrt(impulse.x**2 + impulse.y**2 + impulse.z**2)
-        self.history.append((event.frame, intensity))
-        if len(self.history) > 4000:
-            self.history.pop(0)
-    
-    def destroy(self):
-        if self.sensor:
-            self.sensor.destroy()
-
-
-# ==============================================================================
-# -- LaneInvasionSensor --------------------------------------------------------
-# ==============================================================================
-
-
-class LaneInvasionSensor(object):
-    def __init__(self, parent_actor):
-        self.sensor = None
-        self._parent = parent_actor
-        world = self._parent.get_world()
-        bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
-        self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
-        # We need to pass the lambda a weak reference to self to avoid circular
-        # reference.
-        weak_self = weakref.ref(self)
-        self.sensor.listen(lambda event: LaneInvasionSensor._on_invasion(weak_self, event))
-
-    @staticmethod
-    def _on_invasion(weak_self, event):
-        self = weak_self()
-        if not self:
-            return
-        lane_types = set(x.type for x in event.crossed_lane_markings)
-        text = ['%r' % str(x).split()[-1] for x in lane_types]
-    
-    def destroy(self):
-        if self.sensor:
-            self.sensor.destroy()
-
-# ==============================================================================
-# -- GnssSensor --------------------------------------------------------
-# ==============================================================================
-
-
-class GnssSensor(object):
-    def __init__(self, parent_actor):
-        self.sensor = None
-        self._parent = parent_actor
-        self.lat = 0.0
-        self.lon = 0.0
-        world = self._parent.get_world()
-        bp = world.get_blueprint_library().find('sensor.other.gnss')
-        self.sensor = world.spawn_actor(bp, carla.Transform(carla.Location(x=1.0, z=2.8)), attach_to=self._parent)
-        # We need to pass the lambda a weak reference to self to avoid circular
-        # reference.
-        weak_self = weakref.ref(self)
-        self.sensor.listen(lambda event: GnssSensor._on_gnss_event(weak_self, event))
-
-    @staticmethod
-    def _on_gnss_event(weak_self, event):
-        self = weak_self()
-        if not self:
-            return
-        self.lat = event.latitude
-        self.lon = event.longitude
-
-    def destroy(self):
-        if self.sensor:
-            self.sensor.destroy()
 
 
 # ==============================================================================

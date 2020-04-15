7dd2247145f39bd1a6b8c3c2b4540f0cd342da8c
Arnav Malawade <34730799+malawada@users.noreply.github.com>
2020-04-14 19:57:04 -0700

    added preprocess_scene_graphs and began adding utility for converting to adj matrix/sparse matrix format

--- script/2_preprocess_scene_graphs.py
+++ script/2_preprocess_scene_graphs.py
@@ -1,8 +1,26 @@
 import os, pdb, sys
 sys.path.append(os.path.dirname(sys.path[0]))
 import core.graph_learning.utils
+import pickle as pkl
+from collections import defaultdict
+
 
 #preprocess data for input to graph learning algorithms.
+input_source = "../input/synthesis_data/lane-change-9.8/scenes/"
+
+
+def get_scene_graphs(dir):
+    scenegraphs = defaultdict()
+    _, _ , files = os.walk(dir)
+    for file in files:
+        if ".pkl" in file:
+            scenegraphs[file] = pkl.load(file)
+    return scenegraphs
+
 
 if __name__ == "__main__":
-    
+    scenegraphs = get_scene_graphs(input_source)
+    embeddings = defaultdict()
+    for frame_id, scenegraph in scenegraphs:
+        embeddings[frame_id] = utils.get_node_embeddings(scenegraph)
+    pdb.set_trace()

202907f80c7c7536b6099f16d06dcc5d36375031
Arnav Malawade <34730799+malawada@users.noreply.github.com>
2020-04-14 19:41:46 -0700

    rename

---
+++ script/2_preprocess_scene_graphs.py
@@ -0,0 +1,8 @@
+import os, pdb, sys
+sys.path.append(os.path.dirname(sys.path[0]))
+import core.graph_learning.utils
+
+#preprocess data for input to graph learning algorithms.
+
+if __name__ == "__main__":
+    

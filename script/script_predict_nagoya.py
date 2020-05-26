e6e3aec71f20bc515231e9f73d63a27acf063c82
eslam <eslam@uci.edu>
2020-05-25 21:58:55 -0700

    nagoya predict with metrics

--- script/script_predict_nagoya.py
+++ script/script_predict_nagoya.py
@@ -1,10 +1,10 @@
 import sys
 sys.path.append('../core')
 
+import graph_learning.utils as utils
 from nagoya.dataset import *
-from keras.models import load_model
-
-
+from keras.models import load_model     
+        
 if __name__ == '__main__':
 	root_folder_path = Path('../input/synthesis_data').resolve()
 	raw_image_path = root_folder_path / 'lane-change'
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
 	if not cache_model_path.exists():
 		print("Please train the model first.")
-		return 
+		#return -1
 
 	dataset = load_dataset(masked_image_path, label_table_path)
 	model   = load_model(str(cache_model_path))
@@ -22,4 +22,9 @@ if __name__ == '__main__':
 	'''
 		determine how safe and dangerous each lane change is 
 	'''
+
+	true_label = np.argmax(dataset.risk_one_hot,axis=-1)
+	output = model.predict_proba(dataset.video)
+	utils.get_scoring_metrics(output,true_label,"risk_classification") 
 	print(' safe | dangerous \n', model.predict_proba(dataset.video))
+	import pdb;pdb.set_trace()

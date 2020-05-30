2bc2c11776bd8e4f0f51c03c5579d4c77e5ca181
Aung Thu <aungthu17593@gmail.com>
2020-05-30 13:39:30 -0700

    add lstm to gin

--- core/graph_learning/models/gin.py
+++ core/graph_learning/models/gin.py
@@ -30,6 +30,9 @@ class GIN(nn.Module):
         self.fc1 = Linear(self.hidden_dim, self.hidden_dim)
         self.fc2 = Linear(self.hidden_dim, self.num_classes)
 
+        self.lstm = nn.LSTM(self.num_classes, self.hidden_dim, batch_first=True, bidirectional=True)
+        self.reduce_h = Linear(self.hidden_dim * 2, self.num_classes)
+
     def forward(self, x, edge_index):
         for layer in range(self.num_layers-1):
             x = F.relu(self.gin_convs[layer](x, edge_index))
@@ -39,6 +42,10 @@ class GIN(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.fc2(x)
+
+        x_predicted, (h, c) = self.lstm(x.unsqueeze(0))
+        x = F.relu(self.reduce_h(h.flatten()))
+
         return F.log_softmax(x, dim=-1)
 
 

d98c5fdc9a19572e6a2bff1b20a9bcd204def4dc
Louis Yu <shihyuay@uci.edu>
2020-05-30 14:04:37 -0700

    sag pooling

--- core/graph_learning/models/gin.py
+++ core/graph_learning/models/gin.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.nn import Sequential, Linear, ReLU, LSTM
-from torch_geometric.nn import GINConv
+from torch_geometric.nn import GINConv, SAGPooling
 from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, global_sort_pool
 
 class GIN(nn.Module):
@@ -35,7 +35,6 @@ class GIN(nn.Module):
             x = F.relu(self.gin_convs[layer](x, edge_index))
             x = self.batch_norms[layer](x)
 
-        # x = global_add_pool(x, batch)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.fc2(x)
@@ -80,7 +79,7 @@ class GIN_Graph(nn.Module):
 
 class GIN_Graph_Sequence(nn.Module):
     
-    def __init__(self, args, num_features, num_classes, temporal_type, num_layers):
+    def __init__(self, args, num_features, num_classes, pooling_type, readout_type, temporal_type, num_layers):
         super(GIN_Graph_Sequence, self).__init__()
 
         self.num_features = num_features
@@ -88,6 +87,8 @@ class GIN_Graph_Sequence(nn.Module):
         self.num_layers = num_layers
         self.hidden_dim = 32
 
+        self.pooling_type = pooling_type
+        self.readout_type = readout_type
         self.temporal_type = temporal_type
         
         self.gin_convs = torch.nn.ModuleList()
@@ -104,6 +105,9 @@ class GIN_Graph_Sequence(nn.Module):
         self.fc1 = Linear(self.hidden_dim, self.hidden_dim)
         self.fc2 = Linear(self.hidden_dim, self.num_classes)
 
+        if self.pooling_type == "sagpool":
+            self.pool1 = SAGPooling(self.hidden_dim, ratio=0.8)
+
         self.lstm = LSTM(self.num_classes, self.hidden_dim, batch_first=True, bidirectional=True)
         self.reduce_h = Linear(self.hidden_dim * 2, self.num_classes)
 
@@ -112,7 +116,20 @@ class GIN_Graph_Sequence(nn.Module):
             x = F.relu(self.gin_convs[layer](x, edge_index))
             x = self.batch_norms[layer](x)
 
-        x = global_add_pool(x, batch)
+        if self.pooling_type == "sagpool":
+            x, edge_index, _, batch, perm, score = self.pool1(x, edge_index)
+
+        if self.readout_type == "add":
+            x = global_add_pool(x, batch)
+        elif self.readout_type == "mean":
+            x = global_mean_pool(x, batch)
+        elif self.readout_type == "max":
+            x = global_max_pool(x, batch)
+        elif self.readout_type == "sort":
+            x = global_sort_pool(x, batch, k=100)
+        else:
+            pass
+
         x = F.relu(self.fc1(x))
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.fc2(x)

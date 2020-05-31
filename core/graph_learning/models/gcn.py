4c959577fa63ea689b9c2857a2d3ed27d91eb6ba
Aung Thu <aungthu17593@gmail.com>
2020-05-30 17:03:04 -0700

    merge 3 models

--- core/graph_learning/models/gcn.py
+++ core/graph_learning/models/gcn.py
@@ -10,18 +10,53 @@ from torch_geometric.utils import dense_to_sparse
 from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, global_sort_pool
 
 class GCN(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout):
+    def __init__(self, nfeat, nhid, nclass, dropout, pooling_type=None, readout_type=None, temporal_type=None):
         super(GCN, self).__init__()
+
+        self.pooling_type = pooling_type
+        # switch between average/max/mean/sort.
+        self.readout_type = readout_type
 
         self.gc1 = GCNConv(nfeat, nhid)
         self.gc2 = GCNConv(nhid, nclass)
         self.dropout = dropout
 
-    def forward(self, x, edge_index):
+        if self.pooling_type == "sagpool":
+            self.pool1 = SAGPooling(self.hidden_dim, ratio=0.8)
+        
+        if self.pooling_type == "lstm":
+            self.lstm = nn.LSTM(nclass, nhid, batch_first=True, bidirectional=True)
+            self.fc1 = nn.Linear(2*nhid, nclass)
+
+    def forward(self, x, edge_index, batch=None):
         ''' graphs_in_batch is a list of graph instances; '''
         x = F.relu(self.gc1(x, edge_index))
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, edge_index)
+
+        if self.pooling_type == "sagpool":
+            x, edge_index, _, batch, perm, score = self.pool1(x, edge_index)
+
+        if self.readout_type == "add":
+            x = global_add_pool(x, batch)
+        elif self.readout_type == "mean":
+            x = global_mean_pool(x, batch)
+        elif self.readout_type == "max":
+            x = global_max_pool(x, batch)
+        elif self.readout_type == "sort":
+            x = global_sort_pool(x, batch, k=100)
+        else:
+            pass
+        
+        if self.temporal_type == "mean":
+            x = x.mean(axis=0)
+        elif self.temporal_type == "lstm":
+            x_predicted, (h, c) = self.lstm(x.unsqueeze(0))
+            x = self.fc1(h.flatten())
+            x = F.relu(x)
+        else:
+            pass
+        
         return F.log_softmax(x, dim=1)
 
 
--- core/graph_learning/models/gin.py
+++ core/graph_learning/models/gin.py
@@ -8,7 +8,7 @@ from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_poo
 
 class GIN(nn.Module):
     
-    def __init__(self, args, num_features, num_classes, num_layers):
+    def __init__(self, args, num_features, num_classes, num_layers, pooling_type, readout_type, temporal_type):
         super(GIN, self).__init__()
 
         self.num_features = num_features
@@ -18,6 +18,17 @@ class GIN(nn.Module):
 
         self.gin_convs = torch.nn.ModuleList()
         self.batch_norms = torch.nn.ModuleList()
+
+        self.pooling_type = pooling_type
+        self.readout_type = readout_type
+        self.temporal_type = temporal_type
+
+        if self.pooling_type == "sagpool":
+            self.pool1 = SAGPooling(self.hidden_dim, ratio=0.8)
+
+        if self.temporal_type == "lstm":
+            self.lstm = LSTM(self.num_classes, self.hidden_dim, batch_first=True, bidirectional=True)
+            self.reduce_h = Linear(self.hidden_dim * 2, self.num_classes)
 
         for layer in range(self.num_layers-1):
             if layer == 0:
@@ -30,14 +41,36 @@ class GIN(nn.Module):
         self.fc1 = Linear(self.hidden_dim, self.hidden_dim)
         self.fc2 = Linear(self.hidden_dim, self.num_classes)
 
-    def forward(self, x, edge_index):
+    def forward(self, x, edge_index, batch=None):
         for layer in range(self.num_layers-1):
             x = F.relu(self.gin_convs[layer](x, edge_index))
             x = self.batch_norms[layer](x)
 
+        if self.pooling_type == "sagpool":
+            x, edge_index, _, batch, perm, score = self.pool1(x, edge_index, batch=batch)
+
+        if self.readout_type == "add":
+            x = global_add_pool(x, batch)
+        elif self.readout_type == "mean":
+            x = global_mean_pool(x, batch)
+        elif self.readout_type == "max":
+            x = global_max_pool(x, batch)
+        elif self.readout_type == "sort":
+            x = global_sort_pool(x, batch, k=100)
+        else:
+            pass
+
         x = F.relu(self.fc1(x))
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.fc2(x)
+
+        if self.temporal_type == "mean":
+            x = x.mean(axis=0)
+        elif self.temporal_type == "lstm":
+            x_predicted, (h, c) = self.lstm(x.unsqueeze(0))
+            x = F.relu(self.reduce_h(h.flatten()))
+        else:
+            pass
 
         return F.log_softmax(x, dim=-1)
 
@@ -90,7 +123,7 @@ class GIN_Graph(nn.Module):
             x = global_sort_pool(x, batch, k=100)
         else:
             pass
-        
+
         x = F.relu(self.fc1(x))
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.fc2(x)

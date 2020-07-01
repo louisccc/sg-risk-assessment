128f26692f1c71dce0735fc9005ade9593f88b36
aungthu17593 <aungthu17593@gmail.com>
2020-06-30 17:01:59 -0700

    save and visualize metrics

--- script/3_train_graph_learning.py
+++ script/3_train_graph_learning.py
@@ -6,21 +6,23 @@ from core.dynkg_trainer import *
 import pandas as pd
 import numpy as np
 
-def train_dynamic_kg(args, iterations=2):
+def train_dynamic_kg(args, iterations=1):
     ''' Training the dynamic kg algorithm with different attention layer choice.'''
     
     outputs = []
     labels = []
+    metrics = []
 
     for i in range(iterations):
         trainer = DynKGTrainer(args)
         trainer.build_model()
         trainer.train()
-        output, label = trainer.evaluate()
+        output, label, metric = trainer.evaluate()
 
-        outputs += output.cpu().numpy().tolist()
-        labels  += label.tolist()
-    
+        outputs += output
+        labels  += label
+        metrics.append(metric)
+
     # Store the prediction results. 
     store_path = trainer.config.input_base_dir
     outputs_pd = pd.DataFrame(outputs)
@@ -30,8 +32,7 @@ def train_dynamic_kg(args, iterations=2):
     outputs_pd.to_csv(store_path / "dynkg_training_outputs.tsv", sep="\t", header=False, index=False)
     
     # Store the metric results. 
-    metrics = get_scoring_metrics(outputs, labels, "dynkg_classification")
-    metrics_pd = pd.DataFrame(metrics, index=[0])
+    metrics_pd = pd.DataFrame(metrics[-1]['test'], index=[0])
     metrics_pd.to_csv(store_path / "dynkg_classification_metrics.csv", header=True)
 
 

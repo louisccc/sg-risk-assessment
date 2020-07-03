6e03a0f067c6ac3a38f21d2aefd943f28897f626
aungthu17593 <aungthu17593@gmail.com>
2020-07-02 21:21:17 -0700

    build lane dict

--- dataset_preparation/lane_change_recorder.py
+++ dataset_preparation/lane_change_recorder.py
@@ -119,10 +119,10 @@ class LaneChangeRecorder:
             self.client.apply_batch_sync([carla.command.SetAutopilot(self.ego, False)], True)
               
         if self.lane_changing:
-            ####### collect the lane invasion data. 
-            ## pull the result from self.sensors_dict["lane_invasion"] 
-            # and update to self.extracotr using API?
-            self.extractor.extract_frame(self.carla_world, self.map, frame_num)
+            lane_invasion = self.sensors_dict["lane_invasion"].is_invading_lane(frame_num)
+            self.extractor.extract_frame(self.carla_world, self.map, frame_num, \
+                                         lane_invasion=lane_invasion,\
+                                         lane_change_direction=self.lane_change_direction)
             success = self.lane_change_controller.update()
             if success == py_trees.common.Status.SUCCESS or self.tick_count > 350:
                 #write to metadata file
@@ -135,7 +135,7 @@ class LaneChangeRecorder:
                     
                     file.write(json.dumps(metadata_dict))
 
-
+                self.extractor.export_data()
                 self.lane_changing = False
                 print('set set_autopilot back to true')
                 self.client.apply_batch_sync([carla.command.SetAutopilot(self.ego, True)], True)
@@ -157,12 +157,9 @@ class DataExtractor(object):
         self.framedict=defaultdict()
         self.ego = ego
 
-    def extract_frame(self, world, map1, frame):
-        # utilities
+    def extract_frame(self, world, map1, frame, lane_invasion=False, lane_change_direction="left"):
         t = self.ego.get_transform()
         ego_location = self.ego.get_location()
-        # velocity = lambda l: (3.6 * math.sqrt(l.x**2 + l.y**2 + l.z**2))
-        # dv = lambda l: (3.6 * math.sqrt((l.x-v.x)**2 + (l.y-v.y)**2 + (l.z-v.z)**2))
         distance = lambda l: math.sqrt((l.x - t.location.x)**2 + (l.y - t.location.y)**2 + (l.z - t.location.z)**2)
 
         vehicles = world.get_actors().filter('vehicle.*')
@@ -180,81 +177,66 @@ class DataExtractor(object):
         waypoint = map1.get_waypoint(ego_location, project_to_road=True, lane_type=(carla.LaneType.Driving | carla.LaneType.Shoulder | carla.LaneType.Sidewalk))
 
         ego_lane = waypoint
-        left_lanes = []
-        right_lanes = [] 
+        ## build the new lane dictionary and systems. 
+        left_lanes = build_lanes(waypoint)[::-1]
+        right_lanes = build_lanes(waypoint, direction="right")
+        lanes = left_lanes + [build_dict_lane(ego_lane)] + right_lanes
+        #### dict for each lane. 
         
-        left_lane = waypoint 
-        while True: 
-            lane = left_lane.get_left_lane()
-            if lane is None:
-                break 
-            left_lanes.append(("lane", lane))
-            if lane.lane_type in [carla.LaneType.Shoulder, carla.LaneType.Sidewalk]:
-                break
-            if left_lane.lane_id * lane.lane_id < 0: ## special handling.
-                break
-            left_lane = lane
-            
-        right_lane = waypoint
-        while True:
-            lane = right_lane.get_right_lane()
-            if lane is None:
-                break
-            right_lanes.append(("lane", lane))
-            # print("right", lane.lane_type, lane.lane_change, lane.lane_id)
-
-            if lane.lane_type in [carla.LaneType.Shoulder, carla.LaneType.Sidewalk]:
-                break
-            if right_lane.lane_id * lane.lane_id < 0:
-                break
-            right_lane = lane
-                    
-        lanedict['left_lanes'] = []
-        for name, lane in left_lanes:
-            single_lane_dict = {
-                'lane_id': lane.lane_id,
-                'lane_type': lane.lane_type, 
-                'lane_width': lane.lane_width, 
-                'right_lane_color': lane.right_lane_marking.color, 
-                'left_lane_color': lane.left_lane_marking.color,
-                'right_lane_marking_type': lane.right_lane_marking.type, 
-                'left_lane_marking_type': lane.left_lane_marking.type,
-                'lane_change': lane.lane_change,
-                'is_junction': lane.is_junction,
+        def build_dict_lane_single(lane_waypoint):
+            return {
+                'lane_id': lane_waypoint.lane_id,
+                'road_id': lane_waypoint.road_id, 
+                'lane_type': lane_waypoint.lane_type, 
+                'lane_width': lane_waypoint.lane_width, 
+                'right_lane_marking_type': lane_waypoint.right_lane_marking.type, 
+                'left_lane_marking_type': lane_waypoint.left_lane_marking.type,
+                'lane_change': lane_waypoint.lane_change,
+                'is_junction': lane_waypoint.is_junction,
+                ### adding transform if possible.
             }
-            lanedict['left_lanes'].append(single_lane_dict)
-        
-        lanedict['ego_lane'] = {
-            'lane_id': ego_lane.lane_id,
-            'lane_type': ego_lane.lane_type, 
-            'lane_width': ego_lane.lane_width, 
-            'right_lane_color': ego_lane.right_lane_marking.color, 
-            'left_lane_color': ego_lane.left_lane_marking.color,
-            'right_lane_marking_type': ego_lane.right_lane_marking.type, 
-            'left_lane_marking_type': ego_lane.left_lane_marking.type,
-            'lane_change': ego_lane.lane_change,
-            'is_junction': ego_lane.is_junction,
-        }
 
-        lanedict['right_lanes'] = []
-        
-        for name, lane in right_lanes:        
-            single_lane_dict = {
-                'lane_id': lane.lane_id,
-                'lane_type': lane.lane_type, 
-                'lane_width': lane.lane_width, 
-                'right_lane_color': lane.right_lane_marking.color, 
-                'left_lane_color': lane.left_lane_marking.color,
-                'right_lane_marking_type': lane.right_lane_marking.type, 
-                'left_lane_marking_type': lane.left_lane_marking.type,
-                'lane_change': lane.lane_change,
-                'is_junction': lane.is_junction,
-            }
-            lanedict['right_lanes'].append(single_lane_dict)
+        def build_dict_lane(lane_waypoint, distance=100):
+            ## called by each neighboring lane.
+            lane_dict = {}
+            lane_dict["info"] = build_dict_lane_single(lane_waypoint)
+            lane_dict["next"] = [build_dict_lane_single(next_waypoint) for next_waypoint in lane_waypoint.next(distance)]
+            lane_dict["prev"] = [build_dict_lane_single(next_waypoint) for next_waypoint in lane_waypoint.previous(distance)]
+            return lane_dict
 
-        lanedict['road_id'] = waypoint.road_id
+        def build_lanes(src_lane, direction="left"):
+            lanes = []
+            cur_lane = src_lane # starting the src_lane (ego)
+            while True:
+                lane = cur_lane.get_left_lane() if direction == "left" else cur_lane.get_right_lane()
+                if lane is None:
+                    break 
+                if lane.lane_type in [carla.LaneType.Shoulder, carla.LaneType.Sidewalk]:
+                    break
+                if cur_lane.lane_id * lane.lane_id < 0: ## special handling.
+                    break
+                lanes.append(build_dict_lane(lane))
+                cur_lane = lane
+            return lanes
+        # 1. build the road topology based where the ego is at. 
+        # 2. build the lane idx by our own. not using the opendriving idx.
+        #    also we store waypoints of next(100) and previous(100) for each lane. 
+
+        # lane 0: [current, next1, next2, previous 1, previous 2] next(50), 
+        # lane 1: [current, next1, next2, previous 1, previous 2]
+        # lane 2: ego lane [current, next1, next2, previous 1, previous 2 
+
+        lanedict['lanes'] = lanes
+        lanedict['ego_lane_idx'] = len(left_lanes)
+
         egodict = get_vehicle_attributes(self.ego, waypoint)
-        
+        if lane_invasion:
+            if lane_change_direction == "left":
+                lane_id = lanedict['ego_lane_idx'] - 1
+            else:
+                lane_id = lanedict['ego_lane_idx'] + 1
+            egodict["invading_lane"] = lane_id
+
         # export data from surrounding vehicles
         if len(vehicles) > 1:
             for vehicle in vehicles:
@@ -262,6 +244,7 @@ class DataExtractor(object):
                 if vehicle.id != self.ego.id and distance(vehicle.get_location()) < 100:
                     vehicle_wp = map1.get_waypoint(vehicle.get_location(), project_to_road=True, lane_type=(carla.LaneType.Driving | carla.LaneType.Shoulder | carla.LaneType.Sidewalk))
                     actordict[vehicle.id] = get_vehicle_attributes(vehicle, vehicle_wp)
+                    ## append the lane_id
     
         for p in pedestrians:
             if p.get_location().distance(self.ego.get_location())<100:
@@ -277,11 +260,8 @@ class DataExtractor(object):
                 signdict[s.id]=get_actor_attributes(s)
 
         self.framedict[frame]={"ego": egodict,"actors": actordict,"pedestrians": peddict,"trafficlights": lightdict,"signs": signdict,"lane": lanedict}
-
-        self.export_data()
         
     def export_data(self):
-        if len(self.framedict)==2:
-            with open(self.output_dir / (str(list(self.framedict.keys())[0]) + '-' + str(list(self.framedict.keys())[len(self.framedict)-1])+'.txt'), 'w') as file:
-                file.write(json.dumps(self.framedict))
-            self.framedict.clear()
+        with open(self.output_dir / (str(list(self.framedict.keys())[0]) + '-' + str(list(self.framedict.keys())[len(self.framedict)-1])+'.txt'), 'w') as file:
+            file.write(json.dumps(self.framedict))
+        self.framedict.clear()

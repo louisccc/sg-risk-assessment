3e0c0e41d8afa89dc52838a64528c6f36bc7b11b
Louis Yu <shihyuay@uci.edu>
2020-07-03 21:03:41 -0700

    model adjustment.

--- core/mrgcn.py
+++ core/mrgcn.py
@@ -18,9 +18,6 @@ class MRGCN(nn.Module):
         #self.num_layers = config.num_layers #TODO: remove if unnecessary
         self.hidden_dim = config.hidden_dim
 
-        self.gin_convs = torch.nn.ModuleList()
-        self.batch_norms = torch.nn.ModuleList()
-
         self.pooling_type = config.pooling_type
         self.readout_type = config.readout_type
         self.temporal_type = config.temporal_type
@@ -29,13 +26,12 @@ class MRGCN(nn.Module):
 
         self.conv1 = RGCNConv(self.num_features, self.hidden_dim, self.num_relations, num_bases=30)
         self.conv2 = RGCNConv(self.hidden_dim,   self.hidden_dim, self.num_relations, num_bases=30)
+        self.conv3 = RGCNConv(self.hidden_dim,   self.hidden_dim, self.num_relations, num_bases=30)
 
         if self.pooling_type == "sagpool":
             self.pool1 = SAGPooling(self.hidden_dim, ratio=0.8)
         elif self.pooling_type == "topk":
             self.pool1 = TopKPooling(self.hidden_dim, ratio=0.8)
-        # elif self.pooling_type == "asa":
-        #     self.pool1 = ASAPooling(self.hidden_dim, ratio=0.8)
 
         if "lstm" in self.temporal_type:
             self.lstm = LSTM(self.hidden_dim, self.hidden_dim, batch_first=True, bidirectional=True)
@@ -45,26 +41,25 @@ class MRGCN(nn.Module):
         self.fc1 = Linear(self.hidden_dim, self.hidden_dim)
         self.fc2 = Linear(self.hidden_dim, self.num_classes)
         self.bn1 = torch.nn.BatchNorm1d(self.num_features)
+        self.bn2 = torch.nn.BatchNorm1d(self.hidden_dim)
+        self.bn3 = torch.nn.BatchNorm1d(self.hidden_dim)
 
 
     def forward(self, x, edge_index, edge_attr, batch=None):
         attn_weights = dict()
-        # import pdb; pdb.set_trace()
+        
         x = self.bn1(x)
         x = F.relu(self.conv1(x, edge_index, edge_attr))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.conv2(x, edge_index, edge_attr)
         x = F.relu(self.conv2(x, edge_index, edge_attr))
-        x = F.relu(self.conv2(x, edge_index, edge_attr))
-        x = F.relu(self.conv2(x, edge_index, edge_attr))
-        x = F.relu(self.conv2(x, edge_index, edge_attr))
-        
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.conv3(x, edge_index, edge_attr))
+        x = F.dropout(x, self.dropout, training=self.training)
+
         if self.pooling_type == "sagpool":
             x, edge_index, _, batch, attn_weights['pool_perm'], attn_weights['pool_score'] = self.pool1(x, edge_index, edge_attr=edge_attr, batch=batch)
         elif self.pooling_type == "topk":
             x, edge_index, _, batch, attn_weights['pool_perm'], attn_weights['pool_score'] = self.pool1(x, edge_index, edge_attr=edge_attr, batch=batch)
-        # elif self.pooling_type == "asa":
-        #     x, edge_index, _, batch, attn_weights['pool_perm'] = self.pool1(x, edge_index, batch=batch)
         else: 
             pass
 
@@ -79,8 +74,10 @@ class MRGCN(nn.Module):
         else:
             pass
 
+        x = F.normalize(x, dim=-1, p=1)
+        x = self.bn2(x)
         if self.temporal_type == "mean":
-            x = x.mean(axis=0)
+            x = F.relu(x.mean(axis=0))
         elif self.temporal_type == "lstm_last":
             x_predicted, (h, c) = self.lstm(x.unsqueeze(0))
             x = F.relu(self.fc3(h.flatten()))
@@ -90,11 +87,12 @@ class MRGCN(nn.Module):
         elif self.temporal_type == "lstm_attn":
             x_predicted, (h, c) = self.lstm(x.unsqueeze(0))
             x, attn_weights['lstm_attn_weights'] = self.attn(h.view(1,1,-1), x_predicted)
-            x = self.fc3(x.flatten())
+            x = F.relu(self.fc3(x.flatten()))
         else:
             pass
-            
+        
         x = F.relu(self.fc1(x))
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.fc2(x)
+        
         return F.log_softmax(x, dim=-1)

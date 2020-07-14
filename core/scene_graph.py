d8cc92a1474eb40a8be443bd2ba051420facd184
Louis Yu <shihyuay@uci.edu>
2020-07-14 10:04:21 -0700

    change field of view filtering.

--- core/scene_graph.py
+++ core/scene_graph.py
@@ -79,7 +79,7 @@ class SceneGraph:
             length_product = math.sqrt(x1**2+y1**2) + math.sqrt(x2**2+y2**2)
             degree = math.degrees(math.acos(inner_product / length_product))
             
-            if degree <= 80 or (degree >=280 and degree <= 360):
+            if degree <= 75 or (degree >=285 and degree <= 360):
                 n = Node(actor_id, attr, None)   #using the actor key as the node name and the dict as its attributes.
                 n.name = self.relation_extractor.get_actor_type(n).name.lower() + ":" + actor_id
                 n.type = self.relation_extractor.get_actor_type(n).value

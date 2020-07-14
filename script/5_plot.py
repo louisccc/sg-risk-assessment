cce9c17db2dabaa27ae2091e458db4daf64b88ad
aungthu17593 <aungthu17593@gmail.com>
2020-07-14 13:45:33 -0700

    gui for visualizing scenegraph

--- script/5_plot.py
+++ script/5_plot.py
@@ -1,22 +1,106 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from matplotlib import rcParams
+from tkinter import *
+from PIL import Image, ImageTk
+from pathlib import Path
 
-# figure size in inches optional
-rcParams['figure.figsize'] = 11 ,8
+class GUI():
+    def __init__(self, root_dir, graph_type="carla_visualize/*.png"):
+        self.root_dir = Path(root_dir).resolve()
+        self.folder_paths =  [f for f in self.root_dir.iterdir() if f.stem.isnumeric()]
+        self.folder_paths = sorted(self.folder_paths, key=lambda x : int(x.stem))
 
-# read images
-img_A = mpimg.imread(r'Z:\louisccc\av\synthesis_data\new_recording_3\0\carla_visualize\17794852.png')
-img_B = mpimg.imread(r'Z:\louisccc\av\synthesis_data\new_recording_3\0\raw_images\17794852.jpg')
+        self.lane_change_index = 0
+        self.frame_index = 0
+        self.graph_index = 0
+        self.graph_type = graph_type
 
-# display images
-fig, ax = plt.subplots(1,2)
-# plt.axis('off')
-ax[0].set_axis_off()
-ax[0].imshow(img_A);
-ax[1].set_axis_off()
-ax[1].imshow(img_B);
+        self.root = Tk()
+        self.canvas = Canvas(self.root, width = 400, height = 300)
+        self.canvas.pack()
+        
+        self.load_paths()
+        self.load_images()
 
+        self.raw_image = ImageTk.PhotoImage(self.raw_ims[0])
+        self.graph_image = ImageTk.PhotoImage(self.graph_ims[0])
 
+        self.raw_label = Label(self.canvas, image=self.raw_image)
+        self.raw_label.grid(row=0, column=0, columnspan=2)
+        self.graph_label = Label(self.canvas, image=self.graph_image)
+        self.graph_label.grid(row=0, column=2, columnspan=2)
 
-plt.show()
+        Button(self.canvas, text='Prev Clip', command=self.prevClip).grid(row=1, column=0)
+        Button(self.canvas, text='Prev Frame', command=self.prevFrame).grid(row=1, column=1)
+        Button(self.canvas, text='Next Frame', command=self.nextFrame).grid(row=1, column=2)
+        Button(self.canvas, text='Next Clip', command=self.nextClip).grid(row=1, column=3)
+    
+    def load_paths(self):
+        self.graph_paths = list(self.folder_paths[self.lane_change_index].glob(self.graph_type))
+        tmp_paths = list(self.folder_paths[self.lane_change_index].glob("raw_images/*.jpg")) + list(self.folder_paths[0].glob("raw_images/*.png")) 
+        self.image_paths = []
+        for idx, path in enumerate(tmp_paths):
+            if int(path.stem) == int(self.graph_paths[idx].stem):
+                self.image_paths.append(path)
+
+    def get_image(self, path):
+        image = Image.open(path)
+        image = image.resize((400, 300), Image.ANTIALIAS)
+        return image
+
+    def load_images(self):
+        self.raw_ims = [self.get_image(path) for path in self.image_paths]
+        self.graph_ims = [self.get_image(path) for path in self.graph_paths]
+
+    def destroy_images(self):
+        for im in self.raw_ims:
+            del im
+        for im in self.graph_ims:
+            del im
+
+    def update_title(self):
+        self.root.title("Lane Change ID: {} / {}, Frame: {} / {}".format(self.folder_paths[self.lane_change_index].stem, 
+                                                                        self.folder_paths[-1].stem, self.frame_index, 
+                                                                        len(self.graph_paths) - 1))
+
+    def prevClip(self):
+        if self.lane_change_index > 0:
+            self.lane_change_index -= 1
+            self.update_clip()
+
+    def nextClip(self):
+        if self.lane_change_index < len(self.folder_paths) - 1:
+            self.lane_change_index += 1
+            self.update_clip()
+
+    def prevFrame(self):
+        if self.frame_index > 0:
+            self.frame_index -= 1
+            self.graph_index -= 1
+            self.update_images()
+            self.update_title()
+
+    def nextFrame(self):
+        if self.frame_index < len(self.image_paths) - 1:
+            self.frame_index += 1
+            self.graph_index += 1
+            self.update_images()
+            self.update_title()
+
+    def update_images(self):
+        self.raw_image.paste(self.raw_ims[self.frame_index])
+        self.graph_image.paste(self.graph_ims[self.graph_index])
+
+    def update_clip(self):
+        self.frame_index = 0
+        self.destroy_images()
+        self.load_paths()
+        self.load_images()
+        self.update_images()
+        self.update_title()
+
+    def start(self):
+        self.update_title()
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    gui = GUI("/home/aung/NAS/louisccc/av/synthesis_data/new_recording_3/")
+    gui.start()

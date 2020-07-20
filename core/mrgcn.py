89f9900f5103f6b3fd235d2d3ca30c098244a976
Louis Yu <shihyuay@uci.edu>
2020-07-20 08:48:51 -0700

    add sagpool layer.

--- core/mrgcn.py
+++ core/mrgcn.py
@@ -6,6 +6,61 @@ from torch.nn import Linear, LSTM
 from torch_geometric.nn import RGCNConv, SAGPooling, TopKPooling, FastRGCNConv
 from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, global_sort_pool
 
+from torch_geometric.nn import GraphConv
+from torch_geometric.nn.pool.topk_pool import topk, filter_adj
+from torch_geometric.utils import softmax
+
+
+class RGCNSAGPooling(torch.nn.Module):
+    def __init__(self, in_channels, num_relations, ratio=0.5, min_score=None,
+                 multiplier=1, nonlinearity=torch.tanh, **kwargs):
+        super(RGCNSAGPooling, self).__init__()
+
+        self.in_channels = in_channels
+        self.ratio = ratio
+        self.gnn = FastRGCNConv(in_channels, 1, num_relations, **kwargs)
+        self.min_score = min_score
+        self.multiplier = multiplier
+        self.nonlinearity = nonlinearity
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.gnn.reset_parameters()
+
+
+    def forward(self, x, edge_index, edge_attr=None, batch=None, attn=None):
+        """"""
+        if batch is None:
+            batch = edge_index.new_zeros(x.size(0))
+
+        attn = x if attn is None else attn
+        attn = attn.unsqueeze(-1) if attn.dim() == 1 else attn
+        score = self.gnn(attn, edge_index, edge_attr).view(-1)
+
+        if self.min_score is None:
+            score = self.nonlinearity(score)
+        else:
+            score = softmax(score, batch)
+
+        perm = topk(score, self.ratio, batch, self.min_score)
+        x = x[perm] * score[perm].view(-1, 1)
+        x = self.multiplier * x if self.multiplier != 1 else x
+
+        batch = batch[perm]
+        edge_index, edge_attr = filter_adj(edge_index, edge_attr, perm,
+                                           num_nodes=score.size(0))
+
+        return x, edge_index, edge_attr, batch, perm, score[perm]
+
+
+    def __repr__(self):
+        return '{}({}, {}, {}={}, multiplier={})'.format(
+            self.__class__.__name__, self.gnn.__class__.__name__,
+            self.in_channels,
+            'ratio' if self.min_score is None else 'min_score',
+            self.ratio if self.min_score is None else self.min_score,
+            self.multiplier)
 
 class MRGCN(nn.Module):
     
@@ -31,20 +86,21 @@ class MRGCN(nn.Module):
         dim = self.hidden_dim
         tot_dim = dim
         for i in range(1, self.num_layers):
-            self.conv.append(FastRGCNConv(dim, dim // 2, self.num_relations).to(config.device))
-            dim = dim // 2
+            self.conv.append(FastRGCNConv(dim, dim * 2, self.num_relations).to(config.device))
+            dim = dim * 2
             tot_dim += dim
 
         if self.pooling_type == "sagpool":
-            self.pool1 = SAGPooling(tot_dim, ratio=config.pooling_ratio)
+            self.pool1 = RGCNSAGPooling(tot_dim, self.num_relations, ratio=config.pooling_ratio)
         elif self.pooling_type == "topk":
             self.pool1 = TopKPooling(tot_dim, ratio=config.pooling_ratio)
 
+        self.fc1 = Linear(tot_dim, self.hidden_dim1)
+        
         if "lstm" in self.temporal_type:
             self.lstm = LSTM(self.hidden_dim1, self.hidden_dim2, batch_first=True)
             self.attn = Attention(self.hidden_dim2)
-
-        self.fc1 = Linear(tot_dim, self.hidden_dim1)
+        
         self.fc2 = Linear(self.hidden_dim2, self.num_classes)
 
 
@@ -75,7 +131,7 @@ class MRGCN(nn.Module):
             pass
 
         x = F.relu(self.fc1(x))
-        
+
         if self.temporal_type == "mean":
             x = F.leaky_relu(x.mean(axis=0))
         elif self.temporal_type == "lstm_last":

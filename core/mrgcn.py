7b5967068a1ee34b182a20298b154f43077643a6
Louis Yu <shihyuay@uci.edu>
2020-07-20 01:07:40 -0700

    model changes.

--- core/mrgcn.py
+++ core/mrgcn.py
@@ -17,6 +17,8 @@ class MRGCN(nn.Module):
         self.num_classes  = config.nclass
         self.num_layers = config.num_layers #defines number of RGCN conv layers.
         self.hidden_dim = config.hidden_dim
+        self.hidden_dim2 = 20
+        self.hidden_dim1 = 50
 
         self.pooling_type = config.pooling_type
         self.readout_type = config.readout_type
@@ -25,30 +27,35 @@ class MRGCN(nn.Module):
         self.dropout = config.dropout
         self.conv = []
         self.conv.append(FastRGCNConv(self.num_features, self.hidden_dim, self.num_relations).to(config.device))
+
+        dim = self.hidden_dim
+        tot_dim = dim
         for i in range(1, self.num_layers):
-            self.conv.append(FastRGCNConv(self.hidden_dim, self.hidden_dim, self.num_relations).to(config.device))
+            self.conv.append(FastRGCNConv(dim, dim // 2, self.num_relations).to(config.device))
+            dim = dim // 2
+            tot_dim += dim
 
         if self.pooling_type == "sagpool":
-            self.pool1 = SAGPooling(self.hidden_dim, ratio=config.pooling_ratio)
+            self.pool1 = SAGPooling(tot_dim, ratio=config.pooling_ratio)
         elif self.pooling_type == "topk":
-            self.pool1 = TopKPooling(self.hidden_dim, ratio=config.pooling_ratio)
+            self.pool1 = TopKPooling(tot_dim, ratio=config.pooling_ratio)
 
         if "lstm" in self.temporal_type:
-            self.lstm = LSTM(self.hidden_dim, self.hidden_dim, batch_first=True, bidirectional=True)
-            self.attn = Attention(self.hidden_dim * 2)
-            self.fc3 = Linear(self.hidden_dim * 2, self.hidden_dim)
+            self.lstm = LSTM(self.hidden_dim1, self.hidden_dim2, batch_first=True)
+            self.attn = Attention(self.hidden_dim2)
 
-        self.fc1 = Linear(self.hidden_dim, self.hidden_dim)
-        self.fc2 = Linear(self.hidden_dim, self.num_classes)
+        self.fc1 = Linear(tot_dim, self.hidden_dim1)
+        self.fc2 = Linear(self.hidden_dim2, self.num_classes)
 
 
     def forward(self, x, edge_index, edge_attr, batch=None):
         attn_weights = dict()
-        
+        outputs = []
         for i in range(self.num_layers):
-            x = F.leaky_relu(self.conv[i](x, edge_index, edge_attr))
-            x = F.dropout(x, self.dropout, training=self.training)
-
+            x = F.relu(self.conv[i](x, edge_index, edge_attr))
+            outputs.append(x)
+        x = torch.cat(outputs, dim=-1)
+        x = F.dropout(x, self.dropout, training=self.training)
         if self.pooling_type == "sagpool":
             x, edge_index, _, batch, attn_weights['pool_perm'], attn_weights['pool_score'] = self.pool1(x, edge_index, edge_attr=edge_attr, batch=batch)
         elif self.pooling_type == "topk":
@@ -67,23 +74,21 @@ class MRGCN(nn.Module):
         else:
             pass
 
+        x = F.relu(self.fc1(x))
+        
         if self.temporal_type == "mean":
             x = F.leaky_relu(x.mean(axis=0))
         elif self.temporal_type == "lstm_last":
             x_predicted, (h, c) = self.lstm(x.unsqueeze(0))
-            x = F.leaky_relu(self.fc3(h.flatten()))
+            x = h.flatten()
         elif self.temporal_type == "lstm_sum":
             x_predicted, (h, c) = self.lstm(x.unsqueeze(0))
-            x = F.leaky_relu(self.fc3(x_predicted.sum(dim=1).flatten()))
+            x = x_predicted.sum(dim=1).flatten()
         elif self.temporal_type == "lstm_attn":
             x_predicted, (h, c) = self.lstm(x.unsqueeze(0))
             x, attn_weights['lstm_attn_weights'] = self.attn(h.view(1,1,-1), x_predicted)
-            x = F.leaky_relu(self.fc3(x.flatten()))
+            x = x.flatten()
         else:
             pass
-        
-        x = F.leaky_relu(self.fc1(x))
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.fc2(x)
-        
-        return F.log_softmax(x, dim=-1)
+                
+        return F.log_softmax(self.fc2(x), dim=-1)
